41e159d18e9486a2d7c630b6f502b934be3af0a7
Georgi Karagogov <georgi.karagogov@gmail.com>
2022-02-07 02:55:55 +0200

    Init

---
+++ src/ClientBuilder/Enums.cs
@@ -0,0 +1,27 @@
+﻿namespace ClientBuilder;
+
+/// <summary>
+/// Type of the application instance.
+/// </summary>
+public enum InstanceType
+{
+    /// <summary>
+    /// Undefined type of module.
+    /// </summary>
+    Undefined = 0,
+
+    /// <summary>
+    /// Web module instance.
+    /// </summary>
+    Web = 1,
+
+    /// <summary>
+    /// Mobile module instance.
+    /// </summary>
+    Mobile = 2,
+
+    /// <summary>
+    /// Desktop module instance.
+    /// </summary>
+    Desktop = 3,
+}
